2c6d852f5252e950e2278033ce1c1ceb922c0847
Martin Meyerhoff <mamhoff@gmail.com>
2019-07-08 21:58:31 +0200

    WIP

--- src/gwp.js
+++ src/gwp.js
@@ -1,2 +1,14 @@
-const foo = 'FOOFOOF'
-module.exports = foo
+const fs = require('fs').promises;
+const neatCsv = require('neat-csv');
+const readData = async (dataFile) => {
+  try {
+    const dataFile = await fs.open(dataFile, 'utf-8');
+    const data = await dataFile.readFile();
+    console.log(data);
+    await dataFile.close();
+    return data();
+  } catch (e) {
+    throw new Error(`Failed to load data file at ${dataFile}`);
+  }
+}
+readData('../data/data.csv');
